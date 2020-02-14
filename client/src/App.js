df3714965cca6b22bd826d10a7e282159867e69f
Michael Ross <mross.j@gmail.com>
2020-02-13 19:22:17 -0500

    refactor App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
-function App() {
-  
-    return (
-    <div className="App">
-      <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			players: []
+		};
+	}
+
+	object = {
+		practice: {
+			number: 1
+		}
+	};
+
+	componentDidMount() {
+		axios
+			.get(`http://localhost:5000/api/footballers`)
+			.then(res => {
+				console.log(res.data);
+				console.table(res.data);
+				this.setState({ players: res.data });
+				console.log(Array.isArray(this.state.players));
+				console.log(Array.isArray(this.object.practice));
+			})
+			.catch();
+	}
+	render() {
+		return (
+			<div className='App'>
+				<h1>
+					Happy building{' '}
+					<span role='img' aria-label='Smiling emoji'>
+						😃
+					</span>
+				</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
